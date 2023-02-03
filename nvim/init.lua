438ed6242b5e31ea36185e6b34f9135f9f950459
mlabrkic <m1brkic23@gmail.com>
2023-02-03 16:21:50 +0100

    telescope-fzf-native make

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,7 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   is_bootstrap = true
   vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
   vim.cmd [[packadd packer.nvim]]
+  vim.api.nvim_echo({ { "packer.nvim is installed", "Type" } }, true, {}) -- jdhao -- mlabrkic
 end
 
 require('packer').startup(function(use)
@@ -29,7 +30,8 @@ require('packer').startup(function(use)
   use { -- Autocompletion
     'hrsh7th/nvim-cmp',
     requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip',
-      'hrsh7th/cmp-buffer' }, -- mlabrkic: cmp-buffer
+      'hrsh7th/cmp-buffer',    -- mlabrkic
+    },
   }
 
   use { -- Highlight, edit, and navigate code
@@ -46,7 +48,7 @@ require('packer').startup(function(use)
 
   -- Git related plugins
   use 'tpope/vim-fugitive'
-  use 'tpope/vim-rhubarb'
+  use 'tpope/vim-rhubarb'  -- rhubarb.vim: GitHub extension for fugitive.vim (:GBrowse)
   use 'lewis6991/gitsigns.nvim'
 
   use 'navarasu/onedark.nvim' -- Theme inspired by Atom
@@ -56,13 +58,13 @@ require('packer').startup(function(use)
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
 
   -- Fuzzy Finder (files, lsp, etc)
-  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' }, }
 
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', }
 
-  -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
-  local has_plugins, plugins = pcall(require, 'custom.plugins')
+  -- Add plugins of your choice, to packer, from nvim/lua/plugins.lua
+  local has_plugins, plugins = pcall(require, 'plugins')
   if has_plugins then
     plugins(use)
   end
